251745f187219a85a60369418adc5f1d74398c79
iva <ivvvpol@gmail.com>
2022-08-25 23:30:50 +0200

    donations done

--- src/index.js
+++ src/index.js
@@ -13,17 +13,16 @@ function getRandomGreeting(arr) {
   return item;
 }
 
-//const greeting = getRandomGreeting(greetings.greetingsArr)
 
-bot.on("polling_error", console.log);
+bot.on('polling_error', console.log);
 
 bot.onText(/\/start/, (msg) => {
   bot.sendPhoto(msg.chat.id, getRandomGreeting(greetings.greetingsArr));
 });
 
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, "здоров шо треба", {
-    "reply_markup": {
+  bot.sendMessage(msg.chat.id, 'здоров шо треба', {
+    'reply_markup': {
       "keyboard": [["шо по блєдінам в моєму регіоні?"], ["дізнатись про втрати русні"], ["більше дохлої русні"]]
     }
   });
@@ -36,7 +35,34 @@ bot.on('message', async (msg) => {
   const display = displayStats(statsLatest)
 
   if (msg.text == 'дізнатись про втрати русні') {
-    bot.sendMessage(chat_id, display, {parse_mode : "HTML"})
+    bot.sendMessage(chat_id, display, { parse_mode: "HTML" })
+  }
+  if (msg.text == 'більше дохлої русні') {
+    bot.sendPhoto(msg.chat.id, 'https://i.pinimg.com/originals/f4/04/e7/f404e72bd17890a0f883c51c7e2e9dc2.jpg')
+    bot.sendMessage(msg.chat.id, "більше донатів на зсу - більше дохлої русні! кому донатимо?", {
+      "reply_markup": {
+        "keyboard": [["повернись живим"], ["восток sos"], ["госпітальєри"], ["фонд притули"], ["офіційний рахунок ЗСУ від нацбанку"]]
+      }
+    })
+  }
+})
+
+bot.on('message', async (msg) => {
+  let chat_id = msg.from.id
+  if (msg.text == 'повернись живим') {
+    bot.sendMessage(chat_id, '<a href=\"https://savelife.in.ua/donate/#donate-army-card-once\">ось посилання на донат для фонда "повернись живим"</a>', { parse_mode: "HTML" });
+  }
+  if (msg.text == 'восток sos') {
+    bot.sendMessage(chat_id, '<a href=\"https://vostok-sos.org/en/i-wanna-help/rekvizyty-dlia-hroshovoho-perekazu/\">ось посилання на донат для фонда "восток sos"</a>', { parse_mode: "HTML" });
+  }
+  if (msg.text == 'госпітальєри') {
+    bot.sendMessage(chat_id, '<a href=\"https://www.hospitallers.life/needs-hospitallers\">ось посилання на донат для фонда "госпітальєри"</a>', { parse_mode: "HTML" });
+  }
+  if (msg.text == 'фонд притули') {
+    bot.sendMessage(chat_id, '<a href=\"https://prytulafoundation.org/en/home/support_page/\">ось посилання на донат для фонда Сергія Притули</a>', { parse_mode: "HTML" });
+  }
+  if (msg.text == 'офіційний рахунок ЗСУ від нацбанку') {
+    bot.sendMessage(chat_id, '<a href=\"https://bank.gov.ua/ua/news/all/natsionalniy-bank-vidkriv-spetsrahunok-dlya-zboru-koshtiv-na-potrebi-armiyi\">ось посилання на офіційний рахунок ЗСУ від нацбанку</a>', { parse_mode: "HTML" });
   }
 })
 
@@ -52,8 +78,4 @@ bot.on('message', async (message) => {
   else if (message.text == 'Русні' || message.text == 'русні' || message.text == 'rusni') {
     bot.sendMessage(chat_id, 'Пєзда')
   }
-  // else if (message.text == 'дізнатись про втрати русні') {
-  //   const statsLatest = await getStatisticsLatest()
-  //   const display = displayStats(statsLatest)
-  // }
 })
